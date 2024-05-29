0c4ccba1eb9a110032a583ce2203a21ea3f710a6
SimranKamboj320 <simrankamboj320@gmail.com>
2024-05-30 01:38:08 +0530

    Count set of bits

---
+++ CountSetBits.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class CountSetBits {
+    public static int CountSetBits(int n){
+        int count = 0;
+        while(n > 0) {
+            if((n & 1) != 0) { //Check our LSB
+                count++;
+            }
+            n = n>>1;
+        }
+        return count;
+    }
+
+    public static void main(String args[]){
+        System.out.println(CountSetBits(10));
+    }    
+}

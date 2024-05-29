a747e390528525313d45c398f49e5cca8c1ce9ad
SimranKamboj320 <simrankamboj320@gmail.com>
2024-05-30 01:39:11 +0530

    Fast exponentiation
    Fast exponentiation using optimized time complexity

---
+++ FastExponentiation.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class FastExponentiation{
+    public static int FastExponentiation(int a, int n){
+        int ans = 1;
+        while(n > 0) {
+            if((n & 1) != 0) { //Check our LSB
+                ans = ans * a;
+            }
+            a = a*a;
+            n = n>>1;
+        }
+        return ans;
+    }
+
+    public static void main(String args[]){
+        System.out.println(FastExponentiation(3, 5));
+    }    
+}
+
+ 
